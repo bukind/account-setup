396f21cdc8faa0dbbaafa801e40c57ed61cd7078
Dmitry Bukin <bukin.dima@gmail.com>
2015-10-14 18:09:03 +0100

    Add lockscreen+suspend command
    This might be useful to bind to a keyboard shortcut

---
+++ bin/lock_suspend.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/gnome-screensaver-command --lock &
+/usr/bin/dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend
